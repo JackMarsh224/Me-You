ece00b556ee7eadd23ad5f617abf51b763b5ab84
JackMarsh224 <jack@jigsawgym.com>
2026-03-27 10:55:42 +0000

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -7,6 +7,9 @@ import { createServer } from "http";
 const app = express();
 const httpServer = createServer(app);
 
+app.get("/", (_req, res) => {
+  res.status(200).send("OK");
+});
 declare module "http" {
   interface IncomingMessage {
     rawBody: unknown;
@@ -92,7 +95,7 @@ app.use((req, res, next) => {
   // Other ports are firewalled. Default to 5000 if not specified.
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = parseInt(process.env.PORT || "5000", 10);
+const port = Number(process.env.PORT) || 3000;
   httpServer.listen(
     {
       port,
